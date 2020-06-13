1aaa20afa5dd42e5756cfee06a4b5e304e53b194
Guido Giuntoli <gagiuntoli@gmail.com>
2020-06-13 23:52:57 +0200

    Adding some definitions

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,38 @@
 
 using namespace std;
 
+enum {
+	EMPTY = 0,
+	PIECE_1, 
+	PIECE_2,
+	PIECE_3,
+	PIECE_4,
+	PIECE_5,
+	PIECE_6,
+	PIECE_7,
+	BORDER
+
+};
+
 constexpr int piecesCodes[1][16] = {
-	{0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+	{       EMPTY, PIECE_1, EMPTY, EMPTY,
+		PIECE_1, PIECE_1, PIECE_1, EMPTY, 
+		EMPTY, EMPTY, EMPTY, EMPTY,
+	       	EMPTY, EMPTY, EMPTY, EMPTY}};
 
 constexpr int colors[9] = {
-	' ' | COLOR_PAIR(1),
-	' ' | COLOR_PAIR(2),
-	' ' | COLOR_PAIR(3),
-	' ' | COLOR_PAIR(4),
-	' ' | COLOR_PAIR(5),
-	' ' | COLOR_PAIR(6),
-	' ' | COLOR_PAIR(7),
-	' ' | COLOR_PAIR(8),
-	' ' | COLOR_PAIR(9)};
+	' ' | COLOR_PAIR(EMPTY),
+	' ' | COLOR_PAIR(PIECE_1),
+	' ' | COLOR_PAIR(PIECE_2),
+	' ' | COLOR_PAIR(PIECE_3),
+	' ' | COLOR_PAIR(PIECE_4),
+	' ' | COLOR_PAIR(PIECE_5),
+	' ' | COLOR_PAIR(PIECE_6),
+	' ' | COLOR_PAIR(PIECE_7),
+	' ' | COLOR_PAIR(BORDER)};
 
-constexpr int BOARD_BORDER = colors[7];
-constexpr int BOARD_BACK = colors[8];
+constexpr int COLS_PER_CELL = 2;
+
 
 class Piece {
 	public:
@@ -77,7 +93,6 @@ class Board {
 		bool checkCollisionVertical();
 		bool checkCollisionToLeft();
 		bool checkCollisionToRight();
-		//void checkCollision();
 };
 
 Board::Board(WINDOW *win, int height, int width)
@@ -87,7 +102,7 @@ Board::Board(WINDOW *win, int height, int width)
 
 	for (int i = 0; i < height; i++) {
 		for (int j = 0; j < width; j++) {
-			data[i * width + j] = (i == height - 1 || j == 0 || j == width - 1) ? BOARD_BORDER : BOARD_BACK;
+			data[i * width + j] = (i == height - 1 || j == 0 || j == width - 1) ? BORDER : EMPTY;
 		}
 	}
 }
@@ -97,13 +112,13 @@ Board::~Board()
 	delete [] data;
 }
 
-
 void Board::refresh()
 {
 	for (int i = 0; i < mHeight; i++) {
 		for (int j = 0; j < mWidth; j++) {
-			//mvwaddch(mWin, i, j, colors[data[i * mWidth + j]]);
-			mvwaddch(mWin, i, j, (data[i * mWidth + j] == 1) ? colors[0] : data[i * mWidth + j]);
+			for (int k = 0; k < COLS_PER_CELL; k++) {
+				mvwaddch(mWin, i, j * COLS_PER_CELL + k, colors[data[i * mWidth + j]]);
+			}
 		}
 	}
 	wrefresh(mWin);
@@ -132,7 +147,7 @@ void Board::deleteOldPosition()
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
 			if (activePiece.data[i * 4 + j] != 0)
-				data[(activePiece.y + i) * mWidth + activePiece.x + j] = BOARD_BACK;
+				data[(activePiece.y + i) * mWidth + activePiece.x + j] = EMPTY;
 		}
 	}
 }
@@ -148,7 +163,7 @@ bool Board::checkCollisionVertical()
 	}
 	for (int j = 0; j < 4; j++) {
 		if (activePiece.data[maxY * 4 + j] != 0 &&
-			data[(activePiece.y + maxY + 1) * mWidth + activePiece.x + j] != BOARD_BACK)
+			data[(activePiece.y + maxY + 1) * mWidth + activePiece.x + j] != EMPTY)
 			return true;
 	}
 	return false;
@@ -165,7 +180,7 @@ bool Board::checkCollisionToLeft()
 	}
 	for (int i = 0; i < 4; i++) {
 		if (activePiece.data[i * 4 + minX] != 0 &&
-			data[(activePiece.y + i) * mWidth + activePiece.x + minX - 1] != BOARD_BACK)
+			data[(activePiece.y + i) * mWidth + activePiece.x + minX - 1] != EMPTY)
 			return true;
 	}
 	return false;
@@ -182,7 +197,7 @@ bool Board::checkCollisionToRight()
 	}
 	for (int i = 0; i < 4; i++) {
 		if (activePiece.data[i * 4 + maxX] != 0 &&
-			data[(activePiece.y + i) * mWidth + activePiece.x + maxX + 1] != BOARD_BACK)
+			data[(activePiece.y + i) * mWidth + activePiece.x + maxX + 1] != EMPTY)
 			return true;
 	}
 	return false;
@@ -239,18 +254,18 @@ int main()
 	int width = 12;
 	int x0 = 10, y0 = 10;
 
-	WINDOW *win = newwin(height, width, y0, x0);
+	WINDOW *win = newwin(height, width * COLS_PER_CELL, y0, x0);
 
 	start_color();
-	init_pair(1, COLOR_YELLOW, COLOR_GREEN);
-	init_pair(2, COLOR_YELLOW, COLOR_RED);
-	init_pair(3, COLOR_YELLOW, COLOR_CYAN);
-	init_pair(4, COLOR_YELLOW, COLOR_BLUE);
-	init_pair(5, COLOR_YELLOW, COLOR_WHITE);
-	init_pair(6, COLOR_YELLOW, COLOR_MAGENTA);
-	init_pair(7, COLOR_YELLOW, COLOR_YELLOW);
-	init_pair(8, COLOR_YELLOW, COLOR_WHITE);
-	init_pair(9, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(PIECE_1, COLOR_YELLOW, COLOR_GREEN);
+	init_pair(PIECE_2, COLOR_YELLOW, COLOR_RED);
+	init_pair(PIECE_3, COLOR_YELLOW, COLOR_CYAN);
+	init_pair(PIECE_4, COLOR_YELLOW, COLOR_BLUE);
+	init_pair(PIECE_5, COLOR_YELLOW, COLOR_WHITE);
+	init_pair(PIECE_6, COLOR_YELLOW, COLOR_MAGENTA);
+	init_pair(PIECE_7, COLOR_YELLOW, COLOR_YELLOW);
+	init_pair(BORDER, COLOR_YELLOW, COLOR_WHITE);
+	init_pair(EMPTY, COLOR_YELLOW, COLOR_BLACK);
 
 	Board board(win, height, width);
         board.addNewPiece();
